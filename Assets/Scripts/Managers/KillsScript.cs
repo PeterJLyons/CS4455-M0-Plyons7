6c9e9e353cccecbf36738bc1d640f4f6ec58862f
Peter Lyons <lyoncub1028@gmail.com>
2017-01-16 21:49:54 -0500

    First Commit
    First Commit. Unity Shooter Tutorial with some personal tweaks for
    CS4455 at Georgia Tech.

---
+++ Assets/Scripts/Managers/KillsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillsScript : MonoBehaviour {
+
+	public static int kills;
+	Text text;
+
+
+	void Awake ()
+	{
+		text = GetComponent <Text> ();
+		kills = 0;
+	}
+		
+	void Update ()
+	{
+		text.text = "Kills: " + kills;
+	}
+}
